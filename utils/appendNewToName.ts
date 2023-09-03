31a598d02d0e04492018d6d6fc2185ccf6a5c6dd
Cole Striler <colestriler@gmail.com>
2023-09-02 22:24:12 -0500

    fix downloading error

--- utils/appendNewToName.ts
+++ utils/appendNewToName.ts
@@ -1,11 +1,17 @@
 export default function appendNewToName(name: string, fileFormat?: string) {
-  let insertPos = name.indexOf(".");
-  let newName = name
-    .substring(0, insertPos)
-    .concat("-new", name.substring(insertPos))
-  ;
-  if (fileFormat) {
-    newName = newName.replace(newName.slice(-3), fileFormat);
+  let newName;
+  try {
+    let insertPos = name.indexOf(".");
+     newName= name
+      .substring(0, insertPos)
+      .concat("-new", name.substring(insertPos))
+    ;
+    if (fileFormat) {
+      newName = newName.replace(newName.slice(-3), fileFormat);
+    }
+  } catch (e) {
+    newName = `download.${fileFormat}`
   }
+
   return newName;
 }
